986182f3e7a2af76b48e915e7eca98f66b30bcee
Tevin Mantock <tmantock@gmail.com>
2017-08-02 16:08:34 -0700

    book page

--- app/models/book.js
+++ app/models/book.js
@@ -1,8 +1,10 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   title: DS.attr('string'),
   releaseYear: DS.attr('date'),
   library: DS.belongsTo('library'),
-  author: DS.belongsTo('author')
+  author: DS.belongsTo('author'),
+  isNotValid: Ember.computed.empty('title')
 });
