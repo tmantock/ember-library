e08df33eb831c92103d5c4b69fa95bd9ff6ba44c
Tevin Mantock <tmantock@gmail.com>
2017-08-01 18:42:51 -0700

    updates

--- app/models/author.js
+++ app/models/author.js
@@ -1,6 +1,8 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  books: DS.hasMany('book')
+  books: DS.hasMany('book', { inverse: 'author' }),
+  isNotValid: Ember.computed.empty('name')
 });
